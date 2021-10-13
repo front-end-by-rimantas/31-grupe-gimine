c408e33ec38a3e78c6d27bec5dc1f2225fe7e4d8
Rimantas <belauzas@yahoo.com>
2021-10-13 21:05:38 +0300

    Realus gimines medis

--- src/gimine.js
+++ src/gimine.js
@@ -2,10 +2,85 @@ const gimine = [
     {
         name: 'Petras',
         age: 99,
+        children: [
+            {
+                name: 'Margaret',
+                age: 87,
+                children: [],
+            },
+        ],
     },
     {
         name: 'Maryte',
         age: 102,
+        children: [
+            {
+                name: 'Saulius',
+                age: 44,
+                children: [],
+            },
+            {
+                name: 'Klajumas',
+                age: 46,
+                children: [
+                    {
+                        name: 'Zose',
+                        age: 66,
+                        children: [
+                            {
+                                name: 'Anupra',
+                                age: 14,
+                                children: [],
+                            },
+                            {
+                                name: 'Liuse',
+                                age: 1000,
+                                children: [],
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        name: 'Ona',
+        age: 50,
+        children: [],
+    },
+    {
+        name: 'Kazys',
+        age: 77,
+        children: [
+            {
+                name: 'Adolfas',
+                age: 55,
+                children: [
+                    {
+                        name: 'Rafaelas',
+                        age: 88,
+                        children: [
+                            {
+                                name: 'Leonardas',
+                                age: 89,
+                                children: [
+                                    {
+                                        name: 'Donatelas',
+                                        age: 90,
+                                        children: [],
+                                    },
+                                    {
+                                        name: 'Mikelandzelas',
+                                        age: 17,
+                                        children: [],
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
     },
 ];
 
